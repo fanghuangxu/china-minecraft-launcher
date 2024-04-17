fa84e0e5e1e789bd85900556e78b9736ec320d14
fanghuangxu <159121972+fanghuangxu@users.noreply.github.com>
2024-04-17 21:11:33 +0800

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,31 +1,16 @@
-import minecraft_launcher_lib
-current_max = 0
-
-        # 这里定义了一个函数，获取当前的状态
-def set_status(status: str):
-            print(status)
-
-        # 这个函数获取了当前的进度
-def set_progress(progress: int):
-            if current_max != 0:
-                # 这条函数打印出了当前进度/最大值
-                print(f"{progress}/{current_max}")
-
-        # 这个函数获取了最大值
-def set_max(new_max: int):
-            # 定义一个全局变量
-            global current_max
-            # 赋值
-            current_max = new_max
-
-        # 这个函数获取了默认的.minecraft文件夹路径
-
-        # 这个dict设定了回调的内容，全部是刚才设定的函数
-callback = {
-            "setStatus": set_status,
-            "setProgress": set_progress,
-            "setMax": set_max
-}
-print(minecraft_launcher_lib.forge.find_forge_version('1.18.2'))
-#fabric-loader-0.15.10-1.16.4 
-#minecraft_launcher_lib.forge.install_forge_version(path=r'D:\mcl3\bbs',versionid=minecraft_launcher_lib.forge.find_forge_version('1.18.2'),callback=callback)
+def get_forge_mod():
+    import requests as w
+    json=w.get('https://fanghuangxu.github.io/mcl_mod/mod.json')
+    json=json.json()
+    mod_list=[]
+    for mod in json['forge']:
+        mod_list.append(mod)
+    return mod_list
+def get_fabric_mod():
+    import requests as w
+    json=w.get('https://fanghuangxu.github.io/mcl_mod/mod.json')
+    json=json.json()
+    mod_list=[]
+    for mod in json['fabric']:
+        mod_list.append(mod)
+    return mod_list
